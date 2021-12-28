85975ee91eb8fa8026f228b5b990995b929560ef
ayoub3bidi <mrayoubabidi@gmail.com>
2021-12-28 13:19:45 +0100

    ✨ Trying TypeScript syntaxe on a simple functions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="app">
-    hello hyrule
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge('30')">change age</button>
   </div>
 </template>
 
@@ -9,9 +11,22 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: number | string) {
+      this.age = age
+      return age
+    }
+  }
 });
 </script>
-
-<style>
-</style>
